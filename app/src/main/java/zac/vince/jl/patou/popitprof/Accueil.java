5418ff0d03be0cd6f951df1c016e2d53a58020e9
Jofrey Luc <luc.jofrey@yahoo.fr>
2018-03-18 13:55:34 +0100

    cleanup

--- app/src/main/java/zac/vince/jl/patou/popitprof/Accueil.java
+++ app/src/main/java/zac/vince/jl/patou/popitprof/Accueil.java
@@ -28,28 +28,6 @@ public class Accueil extends AppCompatActivity implements DashboardLauncher {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_accueil, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
     public void launchDashboard(String surveyName) {
         Intent i = new Intent(this, DashboardActivity.class);
         i.putExtra(DashboardActivity.EXTRA_SURVEYNAME, surveyName);
